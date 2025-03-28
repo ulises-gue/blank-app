ed5aa77358fb7adab0a240616c038bf738880df9
ulises-gue <111939337+ulises-gue@users.noreply.github.com>
2025-03-28 18:46:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,7 +110,7 @@ if file_available == "Si":
                 TLR_price,
             ]
         route_data["Precio por KM"] = np.select(conditions, precio_km_values, default=0)
-        evaluation = pd.DataFrame(soute_data, columns = ["Ruta", "Tipo de Ruta", "Sentido", "Precio MXN Quinta", "Precio por KM", "Utilidad (%)", "Evaluacion"])
+        evaluation = pd.DataFrame(route_data, columns = ["Ruta", "Tipo de Ruta", "Sentido", "Precio MXN Quinta", "Precio por KM", "Utilidad (%)", "Evaluacion"])
         
 
         
